b8c6e8477e745c9ddbc008b85ddf3a09c554f279
Valentin Coulon <Valentin.coulon5@outlook.fr>
2025-09-29 16:17:08 +0200

    Use Ligne interface in FactureComponent class

--- src/app/facture/facture.component.ts
+++ src/app/facture/facture.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { Facture } from '../facture';
 import { Item } from '../item';
+import { Ligne } from '../ligne';
 
 @Component({
   selector: 'app-facture',
@@ -13,17 +14,30 @@ export class FactureComponent {
     marque : 'BrandA', 
     prixUnitaire : 100 
   };
+  
   item2 : Item = { 
     reference : 'B2', 
     marque : 'BrandB', 
     prixUnitaire : 200 
   };
-  
+
+  ligne1 : Ligne = { 
+    item: this.item1, 
+    quantite: 2, 
+    prixLigne: function() { return this.item.prixUnitaire * this.quantite; } 
+  };
+
+  ligne2 : Ligne = { 
+    item: this.item2, 
+    quantite: 1, 
+    prixLigne: function() { return this.item.prixUnitaire * this.quantite; } 
+  };
+
   facture: Facture = {
     client: 'John Doe',
     lignes: [
-      { item: this.item1, quantite: 2, prixLigne: function() { return this.item.prixUnitaire * this.quantite; } },
-      { item: this.item2, quantite: 1, prixLigne: function() { return this.item.prixUnitaire * this.quantite; } }
+      this.ligne1,
+      this.ligne2
     ],
     prixtotal: () => {
       return this.facture.lignes.reduce((total, ligne) => total + ligne.prixLigne(), 0);
